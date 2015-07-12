c8905f19edf569daec219cc71dc74bae620d17c5
WC <15520799603@163.com>
2015-07-12 22:55:35 +0800

    new

--- Project1/Project1/源.cpp
+++ Project1/Project1/源.cpp
@@ -1,1 +1,33 @@
 #include <iostream>
+#include <string>
+#include <memory>
+#include <vector>
+#include <initializer_list>
+using namespace std;
+
+class StrBlob{
+public:
+	typedef vector<string>::size_type size_type;
+	StrBlob(){ ; }
+	StrBlob(initializer_list<string> il);
+	size_type size()const;
+	void push_back(const string & t);
+	void pop_back();
+	string & front();
+	string & back();
+private:
+	shared_ptr<vector<string>> data;
+	void check(size_type i, const string & msg)const;
+
+};
+
+StrBlob::StrBlob(initializer_list<string> il)
+	:data(make_shared<vector<string>>(il))
+{}
+
+StrBlob::size_type StrBlob::size()const
+{
+	return data->size();
+}
+
+void StrBlob::push_back(const string & )
